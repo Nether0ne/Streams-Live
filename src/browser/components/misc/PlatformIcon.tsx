3a87b5b4c03f67f78c66d7c5dd673fa017cd5bad
nether0ne <yanstoyanov2012@gmail.com>
2026-01-27 01:42:43 +0200

    feat: add kick icon
    chore: refactor platform icon component
    
    chore: remove outdate platform integration icon

--- src/browser/components/misc/PlatformIcon.tsx
+++ src/browser/components/misc/PlatformIcon.tsx
@@ -2,25 +2,26 @@ import { FC } from "react";
 import { PlatformName } from "@/common/types/platform";
 import TwitchIcon from "../icons/platforms/Twitch";
 import GoodgameIcon from "../icons/platforms/Goodgame";
+import KickIcon from "../icons/platforms/Kick";
 import { SxProps } from "@mui/material";
-import TrovoIcon from "../icons/platforms/Trovo";
 
 interface PlatformIconProps {
   readonly platformName: PlatformName;
   readonly sx?: SxProps;
 }
 
+const PLATFORM_TO_ICON: Record<PlatformName, FC<{ sx?: SxProps }>> = {
+  [PlatformName.TWITCH]: TwitchIcon,
+  [PlatformName.GOODGAME]: GoodgameIcon,
+  [PlatformName.KICK]: KickIcon,
+};
+
 const PlatformIcon: FC<PlatformIconProps> = ({ platformName, sx }) => {
-  return platformName === PlatformName.TWITCH ? (
-    <TwitchIcon {...{ sx }} />
-  ) : platformName === PlatformName.GOODGAME ? (
-    <GoodgameIcon {...{ sx }} />
-  ) : // TODO: Add more platforms
-  // ) : platformName === PlatformName.TROVO ? (
-  //   <TrovoIcon {...{ sx }} />
-  // ) : platformName === PlatformName.WASD ? (
-  //   <>WASD</>
-  null;
+  const Icon = PLATFORM_TO_ICON[platformName];
+
+  if (!Icon) return null;
+
+  return <Icon {...{ sx }} />;
 };
 
 export default PlatformIcon;
